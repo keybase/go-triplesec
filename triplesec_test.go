717d0ab6dc53accdad411515f64fbf5cce87558b
Joshua Blum <jblum18@gmail.com>
2019-05-23 15:32:21 -0400

    staticcheck

--- triplesec_test.go
+++ triplesec_test.go
@@ -76,6 +76,9 @@ func BenchmarkDecrypt(b *testing.B) {
 	password := []byte("42")
 
 	c, err := NewCipher(password, nil, Version(3))
+	if err != nil {
+		b.Fatal(err)
+	}
 	ciphertext, err := c.Encrypt(plaintext)
 	if err != nil {
 		b.Fatal(err)
@@ -178,6 +181,9 @@ func TestSmallerBufSizes(t *testing.T) {
 func TestVectorV3(t *testing.T) {
 	ciphertext, _ := hex.DecodeString("1c94d7de0000000359a5e5d60f09ebb6bc3fdab6642725e03bc3d51e167fa60327df567476d467f8b6ce65a909b4f582443f230ff10a36f60315ebce1cf1395d7b763c768764207f4f4cc5207a21272f3a5542f35db73c94fbc7bd551d4d6b0733e0b27fdf9606b8a26d45c4b79818791b6ae1ad34c23e58de482d454895618a1528ec722c5218650f8a2f55f63a6066ccf875f46c9b68ed31bc1ddce8881d704be597e1b5006d16ebe091a02e24d569f3d09b0578d12f955543e1a1f1dd75784b8b4cba7ca0bb7044389eb6354cea628a21538d")
 	c, err := NewCipher([]byte("42"), nil, Version(3))
+	if err != nil {
+		t.Error(err)
+	}
 	buf, err := c.Decrypt(ciphertext)
 	if err != nil {
 		t.Error(err)
@@ -202,6 +208,9 @@ func TestVectorV4(t *testing.T) {
 	ciphertext, _ := hex.DecodeString("1c94d7de00000004ab62712b6a43fba017a7a13333b59d3650365fcebded3bd64741a99b2070fa12e4145766afa1b2dbcaca4d2053963f441be82963046766f16a4f82186a9ba7a7cf04f19da9a695a4a7ae9f6036a5d3b456ad97d512af55e61245c9a096db8a4b73cc64491ec67e5381ec14f2ce6f3db922b5cec7cea86305681a0204d6fb9522e7ec8851f8d85c4e4319473c2899ece487324093f144d27ea13355fd9a03a0765afc5f5750152824c6632dfd50bd25ac340aaa6e6cd3664c21d4501ef8b3107c3fa62b9a97d79a9ccc64")
 	key, _ := hex.DecodeString("f6b305811712d06e8723")
 	c, err := NewCipher(key, nil, Version(4))
+	if err != nil {
+		t.Error(err)
+	}
 	buf, err := c.Decrypt(ciphertext)
 	if err != nil {
 		t.Error(err)
